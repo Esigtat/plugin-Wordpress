0b357ee52ab49fbd9daa85db8eb7a974aaa31791
Esigtat <tatiana.bron@divcom.ch>
2019-05-20 09:58:47 +0200

    creation du premier code correction

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -9,5 +9,5 @@ Function mon_plugin_meta_keywords(){
     }
  //ajout d'une action sur 'tb_head' qui appellera mon-plugin_meta_keywords()
 
-add_action('tb_head', 'mon_plugin_meta_keywords' );
+add_action('wp_head', 'mon_plugin_meta_keywords' );
 
