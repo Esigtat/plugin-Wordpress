e7c589b9b5e686f2a024fde1e9c5a28af91c062c
Esigtat <tatiana.bron@divcom.ch>
2019-05-20 09:57:30 +0200

    creation du premier code

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -2,3 +2,12 @@
 /*
 Plugin Name: Mon premier Plugin
 */
+
+// fonction qui affiche la balise meta
+Function mon_plugin_meta_keywords(){
+    echo '<meta name="keywords" content="HTML,CSS,XML,JavaScript">';
+    }
+ //ajout d'une action sur 'tb_head' qui appellera mon-plugin_meta_keywords()
+
+add_action('tb_head', 'mon_plugin_meta_keywords' );
+
