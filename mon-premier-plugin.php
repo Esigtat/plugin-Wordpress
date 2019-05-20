52c2fc7487fa4c3c195f0b5352fd015eaf830724
Esigtat <tatiana.bron@divcom.ch>
2019-05-20 10:31:07 +0200

    creation du code delete post

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -11,3 +11,23 @@ Function mon_plugin_meta_keywords(){
 
 add_action('wp_head', 'mon_plugin_meta_keywords' );
 
+
+//function qui envoie par email les infos lors d'un mail supprimé
+
+function mon_plugin_post_delete_mail($post_id)
+{
+    //récupére les infos de l'article supprimé
+    $post = get_post($post_id);
+    //création du sujet de l'email
+    $sujet= "article supprimé : ".$post->post_title;
+    //création du contenu du mail
+    $message = "contenu de l'article : ".$post->post_content;
+    //envoie mail a l'admistrateur du site
+    wp_mail(get_bloginfo('admin_email'),$sujet, $message);
+       }
+
+
+       //ajout d'une actions sur delete poste qui appelera le plugin
+
+add_action('delete_post', 'mon_plugin_post_delete_mail' );
+
